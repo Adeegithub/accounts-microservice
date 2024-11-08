66295a97a1eed55cb37a5fa19f262352f947b6f0
Adeesha Gunawardana <arunodagunawardana@gmail.com>
2024-11-09 00:56:31 +0530

    Implemented Delete API

--- src/main/java/com/eazybank/accounts/service/IAccountService.java
+++ src/main/java/com/eazybank/accounts/service/IAccountService.java
@@ -5,6 +5,6 @@ import com.eazybank.accounts.dto.CustomerDto;
 public interface IAccountService {
     void createAccount(CustomerDto customerDto);
     CustomerDto fetchAccount(String mobileNumber);
-
     boolean updateAccount(CustomerDto customerDto);
+    boolean deleteAccount(String mobileNumber);
 }
