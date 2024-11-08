45ba523e993644f32da6e3cea1bbf59f5e71aaf4
Adeesha Gunawardana <arunodagunawardana@gmail.com>
2024-11-09 00:24:34 +0530

    Defined a Custom method -> deleteByCustomerId()

--- src/main/java/com/eazybank/accounts/repository/AccountsRepository.java
+++ src/main/java/com/eazybank/accounts/repository/AccountsRepository.java
@@ -1,7 +1,9 @@
 package com.eazybank.accounts.repository;
 
 import com.eazybank.accounts.entity.Accounts;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +12,9 @@ import java.util.Optional;
 public interface AccountsRepository extends JpaRepository<Accounts, Long> {
 
     Optional<Accounts> findByCustomerId(Long aLong);
+
+    @Transactional //When transaction fails in runtime partially modified data will roll back by the Spring Data JPA
+    @Modifying //Tell the Spring framework that this method is going to modify the data,
+    //So please execute this query inside a transaction.
+    void deleteByCustomerId(long customerId);
 }
