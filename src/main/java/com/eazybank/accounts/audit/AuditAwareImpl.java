45ee1ce4bd293a5258f01e5e80f9224c997d5516
Adeesha Gunawardana <arunodagunawardana@gmail.com>
2024-11-09 10:37:14 +0530

    Updated Audit Columns using Spring Data.

---
+++ src/main/java/com/eazybank/accounts/audit/AuditAwareImpl.java
@@ -0,0 +1,16 @@
+package com.eazybank.accounts.audit;
+
+import lombok.NonNull;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component("auditAwareImpl")
+public class AuditAwareImpl implements AuditorAware<String> {
+    @Override
+    @NonNull
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("ACCOUNTS_MS");
+    }
+}
